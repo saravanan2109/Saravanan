032b572023ca236e67df484f755f059d596dbf1d
saravanan2109 <48283952+saravanan2109@users.noreply.github.com>
2019-03-08 18:51:14 +0530

    Add files via upload

---
+++ data types.ipynb.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # NUMBERS
+
+# In[3]:
+
+
+a = 10
+
+
+# In[1]:
+
+
+b = 20
+
+
+# In[4]:
+
+
+a+b
+
+
+# In[6]:
+
+
+type(a)
+
+
+# In[7]:
+
+
+type(b)
+
+
+# In[9]:
+
+
+a = 25
+b = 36
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)
+
+
+# In[13]:
+
+
+d=25.98
+e=90.659
+f=d*e
+print(f,type(f),type(d),type(e))
+#type(f)
+
+
+# In[18]:
+
+
+a=10
+b=20
+
+
+# In[19]:
+
+
+a//b
+
+
+# In[20]:
+
+
+b//a
+
+
+# In[21]:
+
+
+a%b
+
+
+# In[22]:
+
+
+abs(-9067.789)
+
+
+# In[23]:
+
+
+ceil(97.9)
+
+
+# In[24]:
+
+
+strr="ayan bhattacharya"
+strr[:2]
+
+
+# In[25]:
+
+
+strr[2:]
+
+
+# In[26]:
+
+
+strr[:]
+
+
+# In[27]:
+
+
+strr[:-1]
+
+
+# In[28]:
+
+
+strr[::-1]
+
+
+# In[29]:
+
+
+strr[::2]
+
+
+# In[31]:
+
+
+strr1="the quick brown fox jumps over the lazy dog"
+strr1[4:40:3]
+
+
+# In[36]:
+
+
+strr2="hello"
+strr2=strr2+34
+strr2
+
+
+# In[37]:
+
+
+strr2="hello"
+strr2=strr2+"34"
+strr2
+
+
+# In[40]:
+
+
+strr2.isupper()
+
+
+# In[41]:
+
+
+strr2.capitalize()
+
+
+# In[43]:
+
+
+strr2.casefold()
+
+
+# In[48]:
+
+
+help(strr2.casefold)
+
+
+# In[55]:
+
+
+strr2.center
+
+
+# In[56]:
+
+
+help(strr2.center)
+
+
+# In[ ]:
+
+
+
+
